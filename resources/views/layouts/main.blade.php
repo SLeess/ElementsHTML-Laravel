f32a1afcc3146313ea0c17e7c005e9b20a2ce9cc
Le <duraesleandro21@gmail.com>
2023-12-22 16:06:44 -0300

    Em andamento - Página inicial

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,7 @@
 
     <!-- JS Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
+    
     <!-- Fonts -->
 
     <!-- Styles -->
@@ -36,7 +36,26 @@
         </aside>
     </main>
     <footer>
-        
+        <article>
+            <h2>Crie mais um elemento</h2>
+            <button>
+                <span>Criar elemento</span>
+                <span> &rarr; </span>
+            </button>
+        </article>
+        <section class="top">
+            <img src="/img/icon.png" alt="">
+            <ul>
+                <li>
+                    <h3>Resources</h3>
+                    <a href="">Usage</a>
+                    <a href="">Docs</a>
+                    <a href="">Support</a>
+                    <a href="">Hardware</a>
+                </li>
+            </ul>
+        </section>
+        <section class="bottom">&copy; 2023 S'</section>
     </footer>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,41 +3,46 @@
 @section('titulo', 'Menu principal')
 
 @section('conteudo')
-<h1 style="margin-bottom: 20px;">Elementos para Desenvolvimento</h1>
+<div class="dropdown">
+    <h1 id="dropdownMenuButton" style="margin-bottom: 20px;white-space: normal;" class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Elementos para Desenvolvimento</h1>
+    <div class="dropdown-menu p-2" aria-labelledby="dropdownMenuButton" style="z-index: 999;">
+        <p class="introduction">
+            Bem-vindo ao nosso <strong>Sistema de gerenciamento de Elementos Web</strong>! 
+            <br><br>
+            Aqui, você pode encontrar e visualizar uma variedade de elementos prontos para uso em seus projetos. Desde barras de navegação elegantes até botões estilizados, nossa tabela contém uma ampla gama de componentes prontos para integrar às suas páginas web.
+        </p>
+        <p class="highlight">
+            Explore a tabela abaixo para obter detalhes sobre cada elemento, incluindo o tipo, descrição, código CSS e HTML.
+        </p>
+    </div>
+</div>
 
-<p class="introduction">
-    Bem-vindo ao nosso <strong>Sistema de gerenciamento de Elementos Web</strong>! 
-    <br>
-    Aqui, você pode encontrar e visualizar uma variedade de elementos prontos para uso em seus projetos. Desde barras de navegação elegantes até botões estilizados, nossa tabela contém uma ampla gama de componentes prontos para integrar às suas páginas web.
-</p>
-<p class="highlight">
-    Explore a tabela abaixo para obter detalhes sobre cada elemento, incluindo o tipo, descrição, código CSS e HTML.
-</p>
-
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Tipo</th>
-      <th scope="col">Descrição</th>
-      <th scope="col">CSS</th>
-      <th scope="col">HTML</th>
-      <th scope="col">Data de Criação</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach($elements as $element)
-      <tr>
-        <th scope="row">{{ $element->id }}</th>
-        <td>{{ $element->type }}</td>
-        <td>{{ $element->description }}</td>
-        <td>{{ $element->css }}</td>
-        <td>{{ $element->html }}</td>
-        <td>{{ $element->created_at }}</td>
-      </tr>
-    @endforeach
-  </tbody>
-</table>
+<div class="row table-responsive">
+    <table class="table table-striped">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Tipo</th>
+        <th scope="col">Descrição</th>
+        <th scope="col">CSS</th>
+        <th scope="col">HTML</th>
+        <th scope="col">Data de Criação</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($elements as $element)
+        <tr>
+            <th scope="row">{{ $element->id }}</th>
+            <td>{{ $element->type }}</td>
+            <td>{{ $element->description }}</td>
+            <td>{{ $element->css }}</td>
+            <td>{{ $element->html }}</td>
+            <td>{{ $element->created_at }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+    </table>
+</div>
 @endsection
 
 @section('line-styles')
@@ -51,11 +56,14 @@
     }
 
     .highlight {
-        font-size: 26px;
+        font-size: 20px;
         color: var(--cor-navbar);
         font-weight: bold;
         margin-bottom: 30px;
         line-height: 1.6;
+    }
+    .show{
+        display: inline-block;
     }
 </style>
 @endsection
